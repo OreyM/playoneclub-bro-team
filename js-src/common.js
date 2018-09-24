cf6ef786a92c45f383dd093dc6ce55424f8a86c6
OreyM <oreymgt@gmail.com>
2018-09-24 19:01:01 +0300

    Add game page (without mobile) 24-09-2018-17-04-Orey

--- js-src/common.js
+++ js-src/common.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
     Events
     https://owlcarousel2.github.io/OwlCarousel2/docs/api-events.html
      */
-    $('.owl-carousel').owlCarousel({
+    $('.screenshot-carousel').owlCarousel({
         margin: 10, // Type: Number. margin-right(px) on item
         loop: true, // Infinity loop. Duplicate last and first items to get loop illusion
         center: false,  // Center item. Works well with even an odd number of items
@@ -25,7 +25,7 @@ $(document).ready(function(){
         autoWidth: false, // Set non grid content. Try using width style on divs
         startPosition: 0, // Type: Number/String. Start position or URL Hash string like '#id'
         URLhashListener: false, // Listen to url hash changes. data-hash on items is required
-        nav: true, // Show next/prev buttons
+        nav: false, // Show next/prev buttons
         rewind: true, // Go backwards when the boundary has reached.
         navText : ['&#x27;next&#x27;','&#x27;prev&#x27;'], // Type: Array. HTML allowed.
         slideBy: 1, // Type: Number/String
@@ -33,7 +33,7 @@ $(document).ready(function(){
         slideTransition: '', // Type: String.
                             // You can define the transition for the stage
                             // you want to use eg. linear
-        dots : true, // Show dots navigation
+        dots : false, // Show dots navigation
         dotsEach : false, // Type: Number/Boolean. Show dots each x item
         dotData : false, // Used by data-dot content
         lazyLoad: false, // Lazy load images. data-src and data-src-retina for highres.
@@ -41,8 +41,8 @@ $(document).ready(function(){
         lazyLoadEager: 0, // Type: Number. Eagerly pre-loads images to the right (and left
                           // when loop is enabled) based on how many items you want to preload
         autoplay : true, // Autoplay
-        autoplayTimeout: 5000, // Type: Number. Autoplay interval timeout, ms.
-        autoplayHoverPause : false, // Pause on mouse hover.
+        autoplayTimeout: 7000, // Type: Number. Autoplay interval timeout, ms.
+        autoplayHoverPause : true, // Pause on mouse hover.
         smartSpeed: 250, // Type: Number. Speed Calculate. More info to come..
         fluidSpeed : 600, // Type: Number. Speed Calculate. More info to come..
         autoplaySpeed : false, // Type: Number/Boolean. Autoplay speed.
@@ -58,22 +58,107 @@ $(document).ready(function(){
                 items: 1
             },
             600: {
-                items: 2
+                items: 1
             },
             768: {
-                items: 3
+                items: 1
             },
             1000: {
-                items: 4
+                items: 3
             },
             1280: {
-                items: 5
+                items: 4
             }
         },
         responsiveRefreshRate: 200, // Type: Number. Responsive refresh rate.
         responsiveBaseElement: window, // Type: DOM element. Set on any DOM element.
                                       // If you care about non responsive browser (like ie8) then
                                       // use it on main wrapper. This will prevent from crazy resizing.
+        video: false, // Enable fetching YouTube/Vimeo/Vzaar videos.
+        videoHeight: false, // Type: Number/Boolean. Set height for videos.
+        videoWidth: false, // Type: Number/Boolean. Set width for videos.
+        animateOut: false, // Type: String/Boolean. Class for CSS3 animation out.
+        animateIn: false, // Type: String/Boolean. Class for CSS3 animation in.
+        fallbackEasing: 'swing', // Type: String. Easing for CSS2 $.animate
+        info: false, // Type: Function. Callback to retrieve basic information (current item/pages/widths).
+                     // Info function second parameter is Owl DOM object reference.
+        nestedItemSelector: false, // Type: String/Class. Use it if owl items are deep nested inside
+                                   // some generated content. E.g 'youritem'. Dont use dot before class name.
+        itemElement: 'div', // Type: String. DOM element type for owl-item.
+        stageElement: 'div', // Type: String. DOM element type for owl-stage.
+        navContainer: false, // Type: String/Class/ID/Boolean. Set your own container for nav.
+        dotsContainer: false, // Type: String/Class/ID/Boolean. Set your own container for nav.
+        checkVisible: true, // If you know the carousel will always be visible you can set
+                            // `checkVisibility` to `false` to prevent the expensive browser layout
+                            // forced reflow the $element.is(':visible') does.
+    });
+
+    $('.games-carousel').owlCarousel({
+        margin: 10, // Type: Number. margin-right(px) on item
+        loop: true, // Infinity loop. Duplicate last and first items to get loop illusion
+        center: false,  // Center item. Works well with even an odd number of items
+        mouseDrag: true, // Mouse drag enabled
+        touchDrag: true, // Touch drag enabled
+        pullDrag: true, // Stage pull to edge
+        freeDrag: false, // Item pull to edge
+        stagePadding: 0, // Type: Number. Padding left and right on stage (can see neighbours)
+        merge: false, // Merge items. Looking for data-merge='{number}' inside item
+        mergeFit: true, // Fit merged items if screen is smaller than items value
+        autoWidth: false, // Set non grid content. Try using width style on divs
+        startPosition: 0, // Type: Number/String. Start position or URL Hash string like '#id'
+        URLhashListener: false, // Listen to url hash changes. data-hash on items is required
+        nav: false, // Show next/prev buttons
+        rewind: true, // Go backwards when the boundary has reached.
+        navText : ['&#x27;next&#x27;','&#x27;prev&#x27;'], // Type: Array. HTML allowed.
+        slideBy: 1, // Type: Number/String
+                    // Navigation slide by x. 'page' string can be set to slide by page
+        slideTransition: '', // Type: String.
+        // You can define the transition for the stage
+        // you want to use eg. linear
+        dots : false, // Show dots navigation
+        dotsEach : false, // Type: Number/Boolean. Show dots each x item
+        dotData : false, // Used by data-dot content
+        lazyLoad: false, // Lazy load images. data-src and data-src-retina for highres.
+        // Also load images into background inline style if element is not <img>
+        lazyLoadEager: 0, // Type: Number. Eagerly pre-loads images to the right (and left
+                          // when loop is enabled) based on how many items you want to preload
+        autoplay : false, // Autoplay
+        autoplayTimeout: 7000, // Type: Number. Autoplay interval timeout, ms.
+        autoplayHoverPause : true, // Pause on mouse hover.
+        smartSpeed: 250, // Type: Number. Speed Calculate. More info to come..
+        fluidSpeed : 600, // Type: Number. Speed Calculate. More info to come..
+        autoplaySpeed : false, // Type: Number/Boolean. Autoplay speed.
+        navSpeed : false, // Type: Number/Boolean. Navigation speed.
+        dotsSpeed : false, // Type: Number/Boolean. Pagination speed.
+        dragEndSpeed : false, // Type: Number/Boolean. Drag end speed.
+        callbacks: true, // Enable callback events.
+        responsive: {       // Type: Object
+            // Object containing responsive options. Can be set to false to remove
+            // responsive capabilities.
+            // the number of items you want to see on the screen.
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            },
+            1280: {
+                items: 4
+            },
+            1920: {
+                items: 5
+            }
+        },
+        responsiveRefreshRate: 200, // Type: Number. Responsive refresh rate.
+        responsiveBaseElement: window, // Type: DOM element. Set on any DOM element.
+        // If you care about non responsive browser (like ie8) then
+        // use it on main wrapper. This will prevent from crazy resizing.
         video: false, // Enable fetching YouTube/Vimeo/Vzaar videos.
         videoHeight: false, // Type: Number/Boolean. Set height for videos.
         videoWidth: false, // Type: Number/Boolean. Set width for videos.
@@ -123,17 +208,18 @@ $(document).ready(function() {
 // Search
 $(document).ready(function() {
 
-    $("#search").click(function(e) {
+    $("#search, #game-search").click(function(e) {
         e.preventDefault();
-        $("#search-modal").addClass('active');
-        $(".search-games-list").addClass('hiden-result-search');
-        $("#search-opacity").addClass('detected');
+        $("#search-modal").toggleClass('active');
+        $(".search-games-list").toggleClass('hiden-result-search');
+        $("#search-opacity").toggleClass('detected');
+        $("#mobile-menu-btn").toggleClass('hide-mobile');
     });
-    $("#search-opacity").click(function(e) {
-        e.preventDefault();
+    $("#search-opacity, .close-search-mobile").click(function() {
         $("#search-modal").removeClass('active');
         $(".search-games-list").removeClass('hiden-result-search');
         $("#search-opacity").removeClass('detected');
+        $("#mobile-menu-btn").removeClass('hide-mobile');
     });
 });
 
